9a0689f2de5e9e394ce3dc7388f58791848102bf
Will Leinweber <will@bitfission.com>
2009-05-15 03:27:01 -0500

    add readme. cleanup solver.rb

---
+++ README.md
@@ -0,0 +1,7 @@
+sudoku solver
+=============
+author: will leinweber (will@bitfission.com)
+
+A simple sudoku solver using the observer pattern. It probably can't solve hard ones where you have to take guesses and check, but I'm not really sure since I never actually play sudoku.
+
+mit license.
--- solver.rb
+++ solver.rb
@@ -2,25 +2,6 @@ require 'cell'
 require 'group'
 require 'board'
 
-
-
-def manual_solve
-  b = Board.new
-  loop do
-    b.print
-  
-    print "row: "
-    row = gets.to_i
-    print "col: "
-    col = gets.to_i
-    print "val: "
-    val = gets.to_i
-  
-    b.set row, col, val
-  
-  end
-end
-
 def solve
   b = Board.new
   (0..8).each do |row|
